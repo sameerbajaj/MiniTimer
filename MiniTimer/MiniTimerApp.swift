7ddffe8c4a9b7d2cabca2c17cb9873d3d6e28396
sameerbajaj <sameerbajaj24@gmail.com>
2026-03-11 16:23:43 +0530

    Initial Commit

---
+++ MiniTimer/MiniTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniTimerApp.swift
+//  MiniTimer
+//
+//  Created by Sameer Bajaj on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniTimerTests/MiniTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniTimerTests.swift
+//  MiniTimerTests
+//
+//  Created by Sameer Bajaj on 3/11/26.
+//
+
+import Testing
+@testable import MiniTimer
+
+struct MiniTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
